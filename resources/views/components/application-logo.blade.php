c99787054a41f116850d5f4d1fa3bb17a34b2854
Mikusune <72233287+Mikusune@users.noreply.github.com>
2024-09-30 13:27:10 +0700

    template admin lte

--- resources/views/components/application-logo.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="text-center mb-4">
-    <h1 class="text-primary mb-0" style="font-size: 48px;">
-        <i class="fab fa-slack me-2" style="font-size: 64px;"></i> PSAK71
-    </h1>
-</div>
