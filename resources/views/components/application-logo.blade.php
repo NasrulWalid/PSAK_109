3598ee66a1ab53d1e44cd9a3a81cd8781c3f7a21
Nasrul Walid Hidayat <89139644+NasrulWalid@users.noreply.github.com>
2024-10-14 15:59:50 +0700

    modifikasi logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,7 @@
 <div class="text-center mb-4">
-    <h1 class="text-primary mb-0" style="font-size: 48px;">
-        <i class="fab fa-slack me-2" style="font-size: 64px;"></i> PSAK71
+    <h1 class="text-primary mb-0" style="font-size: 44px; display: flex; align-items: center;">
+        <img src="{{ asset('image/pramatech.png') }}" alt="Pramatech Logo" style="height: 60px; margin-right: 10px; background-color: transparent; ">
+        PSAK71
     </h1>
 </div>
+
