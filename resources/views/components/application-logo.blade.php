86a1741c91af87d5c8da38ee846363491e2dcd24
Nasrul Walid Hidayat <89139644+NasrulWalid@users.noreply.github.com>
2024-10-14 16:04:07 +0700

    perubahan font logo psak

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="text-center mb-4">
-    <h1 class="text-primary mb-0" style="font-size: 44px; display: flex; align-items: center;">
+    <h1 class="text-primary mb-0" style=" font-family: 'Roboto', sans-serif;  font-size: 44px; display: flex; align-items: center;">
         <img src="{{ asset('image/pramatech.png') }}" alt="Pramatech Logo" style="height: 60px; margin-right: 10px; background-color: transparent; ">
         PSAK71
     </h1>
